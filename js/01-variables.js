7253473124ee6f9dfd15bdba1847e06f41fc6af1
Florencia Gonzalez <iturbe.mariaflorencia@gmail.com>
2023-10-30 09:50:06 -0300

    declaramos e inicializamos una variable con let y const

---
+++ js/01-variables.js
@@ -0,0 +1,10 @@
+let anioActual = 2023;
+
+console.log(anioActual);
+console.log('Año actual: ' + anioActual);
+
+anioActual = anioActual + 1;
+console.log('Año actual: ' + anioActual);
+
+const url = 'https://universe.rollingcodeschool.com/';
+console.log(url);
