448864eb33d21aeb88c96f9f5e9410b7514c44c1
Florencia Gonzalez <iturbe.mariaflorencia@gmail.com>
2023-11-05 17:30:56 -0300

    le di formato al codigo

--- js/02-condicionales.js
+++ js/02-condicionales.js
@@ -2,14 +2,14 @@
 Entre 16 y 18 es optativo y mayores de 70 
 Entre 18 y 70 años incluidos es obligatorio*/
 
-const edad = parseInt(prompt('Ingrese su edad: '));
+const edad = parseInt(prompt("Ingrese su edad: "));
 
-if(edad >= 16 && edad < 18 || edad > 70){
-    alert('Es optativo votar');
-} else{
-    if(edad >= 18 && edad <= 70){
-        alert('Usted debe votar')
-    } else{
-        alert('Usted no puede votar')
-    }
+if ((edad >= 16 && edad < 18) || edad > 70) {
+  alert("Es optativo votar");
+} else {
+  if (edad >= 18 && edad <= 70) {
+    alert("Usted debe votar");
+  } else {
+    alert("Usted no puede votar");
+  }
 }
